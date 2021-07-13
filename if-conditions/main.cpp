bf8492d7d37d281c5faa6c5b9c1186f595649d59
worachote1 <68600257+worachote1@users.noreply.github.com>
2021-07-13 09:40:34 +0700

    commit-if-conditions

---
+++ if-conditions/main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	int ID ;
+
+	cout << "Enter your ID : " ;
+	cin >> ID ;
+
+	switch (ID)
+
+	{
+		case 0:
+			cout << "\n this user is : timewalker \n";
+			break;
+
+		case 1:
+			cout << "\n this user is : muller \n";
+			break;
+
+		case 2:
+			cout << "\n this user is : veratti \n";
+			break;
+
+		default :
+			cout << "You've typed wrong ID , please" ;
+	}
+
+	return 0;
+}
